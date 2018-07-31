e90b7f48ab52040a9d46acb10d56ab9112193b90
mohanp <mohancmg@gmail.com>
2018-07-31 11:08:55 +0530

    Python: add sequence data types string tuple and list

---
+++ python_demos/variable_sequences.py
@@ -0,0 +1,7 @@
+name = "Alice"
+coordinates = (10.0, 20.0)
+names = ["Alice", "Bob", "Charlie"]
+
+print(f"name: {name}", "Data type: ", type(name))
+print(f"coordinates: {coordinates}", "Data type: ", type(coordinates))
+print(f"names: {names}", "Data type", type(names))
