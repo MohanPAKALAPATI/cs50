534b3f0776c5b8675d4a47227ed002ff00b6cc55
mohanp <mohancmg@gmail.com>
2018-07-31 12:33:40 +0530

    Python: enable import and execute only required function thru __name__

--- python_demos/functions_demo.py
+++ python_demos/functions_demo.py
@@ -2,5 +2,10 @@ def square(x_input):
     return(x_input * x_input)
 
 
-for i in range(10):
-    print("{} squared is {}".format(i, square(i)))
+def main():
+    for i in range(10):
+        print("{} squared is {}".format(i, square(i)))
+
+if __name__ == "__main__":
+    main()                #if function calling name is main
+                           #then main will starts execution
